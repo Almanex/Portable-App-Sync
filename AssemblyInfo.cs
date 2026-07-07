9abd82ec497b8ee8e713dbe90f472fa74f32cbed
Almanex <alex@example.com>
2026-07-07 09:55:25 +0200

    Bump version to 1.1.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 
 [assembly: ComVisible(false)]
 
